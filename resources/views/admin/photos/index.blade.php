8a2228f2010517c21bac2713e061b9b0732f18ed
Belonozhko Artyom <ab@mossebo.ru>
2020-03-30 11:51:38 +0300

    Updates of admin-side Photos CRUD pages.

--- resources/views/admin/photos/index.blade.php
+++ resources/views/admin/photos/index.blade.php
@@ -1,44 +1,46 @@
 @extends('layouts.admin')
 
 @section('page-content')
-    <div class="row">
-        <div class="col-10">
-            <h1>Фотографии</h1>
-        </div>
+    <div class="photo-list">
+        <div class="row">
+            <div class="col-10">
+                <h1>Фотографии</h1>
+            </div>
 
-        <div class="col-2">
-            <a href="{{ route('admin.photos.create') }}" class="btn btn-success">Создать</a>
+            <div class="col-2">
+                <a href="{{ route('admin.photos.create') }}" class="btn btn-success">Создать</a>
+            </div>
         </div>
-    </div>
-    @if(count($photos))
-        <table>
-            <tr>
-                <th>Название</th>
-                <th>Превью</th>
-                <th>Кол-во лайков</th>
-                <th>Редактировать</th>
-                <th>Удалить</th>
-            </tr>
-            @foreach($photos as $photo)
-
+        @if(count($photos))
+            <table>
                 <tr>
-                    <td>{{ $photo->title }}</td>
-                    <td><img src="{{ $photo->getMedia('payload')->first()->getUrl('thumb') }}" alt="{{ $photo->title }}"></td>
-                    <td>{{ $photo->likes->count() }}</td>
-                    <td><a href="{{ route('admin.photos.edit', $photo->id) }}">Редактировать</a></td>
-                    <td>
-                        <form action="{{ route('admin.photos.destroy', $photo->id) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-
-                            <input type="submit" value="Удалить">
-                        </form>
-                    </td>
+                    <th>Название</th>
+                    <th>Превью</th>
+                    <th>Кол-во лайков</th>
+                    <th>Редактировать</th>
+                    <th>Удалить</th>
                 </tr>
-            @endforeach
-        </table>
-        {{ $photos->links() }}
-    @else
-        До сих пор ни одной фотографии!
-    @endif
+                @foreach($photos as $photo)
+
+                    <tr>
+                        <td>{{ $photo->title }}</td>
+                        <td><img src="{{ $photo->getMedia('payload')->first()->getUrl('thumb') }}" alt="{{ $photo->title }}" class="preview"></td>
+                        <td>{{ $photo->likes->count() }}</td>
+                        <td><a href="{{ route('admin.photos.edit', $photo->id) }}">Редактировать</a></td>
+                        <td>
+                            <form action="{{ route('admin.photos.destroy', $photo->id) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+
+                                <input type="submit" value="Удалить">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+            {{ $photos->links() }}
+        @else
+            До сих пор ни одной фотографии!
+        @endif
+    </div>
 @endsection
--- resources/views/admin/photos/show.blade.php
+++ resources/views/admin/photos/show.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.admin')
 
 @section('page-content')
-    <div class="row">
-        <div class="col-10">
-            <h1>Фотография # {{ $photo->id }}</h1>
+    <div class="photo">
+        <div class="row">
+            <div class="col-10">
+                <h1>Фотография # {{ $photo->id }}</h1>
 
+            </div>
+            <div class="col-2">
+                <a href="{{ route('admin.photos.index') }}" class="btn btn-primary">Назад</a>
+            </div>
         </div>
-        <div class="col-2">
-            <a href="{{ route('admin.photos.index') }}" class="btn btn-primary">Назад</a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="form-group">
-            <label for="title">Название</label>
-            <input id="title" name="title" class="form-control" type="text" disabled value="{{ $photo->title }}">
-        </div>
+        <div class="row">
+            <div class="form-group">
+                <label for="title">Название</label>
+                <input id="title" name="title" class="form-control" type="text" disabled value="{{ $photo->title }}">
+            </div>
 
-        <div class="form-group">
-            <label for="image">Файл</label>
-            <img src="{{ asset($photo->getMedia('payload')->first()->getUrl('thumb')) }}" alt="{{ $photo->title }}">
+            <div class="form-group">
+                <label for="image">Файл1</label>
+                <img src="{{ $photo->getMedia('payload')->first()->getUrl('thumb') }}" alt="{{ $photo->title }}" class="preview">
+            </div>
         </div>
     </div>
 @endsection
